46432bfc17c814e19d3ea13cbb0554c6e6477e2f
hstrada <6079947+hstrada@users.noreply.github.com>
2022-03-02 18:34:26 -0300

    enable swagger in env different from prod

--- mercado-livro/src/main/kotlin/com/mercadolivro/config/SwaggerConfig.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/config/SwaggerConfig.kt
@@ -2,12 +2,17 @@ package com.mercadolivro.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 
+/*
+run in every env that is not prod
+ */
+@Profile("!prod")
 @Configuration
 class SwaggerConfig {
 
